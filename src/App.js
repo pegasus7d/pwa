7e9ae83b7ea27bcdffa64ed0ae11bc0bee2e124d
Debayan Biswas <debayanbiswas1111@gmail.com>
2023-05-12 21:46:53 +0530

    added suspense and lazy load

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import ChatArea from "./components/ChatArea";
-
-
+// import ChatArea from "./components/ChatArea";
+import { lazy,Suspense } from "react";
+const ChatArea= lazy(()=>import("./components/ChatArea"))
 function App() {
   return (
     <div>
+      <Suspense fallback={<div>Please wait ....</div>}>
       <ChatArea/>
+      </Suspense>
     </div>
   );
 }
